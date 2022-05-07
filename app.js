0926f2c53729edda6e6a4270b5bb9d95076fe24c
bkcp <bkcp83@gmail.com>
2022-05-07 09:53:55 -0700

    Using Axios to call TVMaze api to get the images of movies searched for. First implementation

---
+++ app.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('searchForm')
+const images = document.querySelector("#images");
+form.addEventListener("submit",function(e){
+    e.preventDefault()
+    const searchTerm = this.elements.query.value;
+    console.log(searchTerm);
+    console.log('submitted')
+    console.dir(this)
+    console.log(this.elements.query.value);
+
+    const fetchData = async(id)=>{
+        try{
+      const returnedShow = await axios.get(`https://api.tvmaze.com/search/shows?q=${id}`);
+      const newImage = document.createElement("img");
+      const returnedImage = returnedShow.data[0].show.image.medium;
+      newImage.setAttribute("src",returnedImage);
+      images.append(newImage);
+        }
+        catch(e){
+            console.log("ERROR:", e);
+        }
+    }
+    
+fetchData(searchTerm);
+  
+})
